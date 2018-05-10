30e21aaad335c965f07f0c3bc5383204b38cf684
注意三观 <xingziyi@aliyun.com>
2018-05-10 10:54:43 +0800

    Add test programs

---
+++ TEST/Network-TEST-client.py
@@ -0,0 +1,15 @@
+#客户端与上一个没有任何改变
+from socket import *
+address='127.0.0.1'   #服务器的ip地址
+port=1124          #服务器的端口号
+buffsize=1024        #接收数据的缓存大小
+s=socket(AF_INET, SOCK_STREAM)
+s.connect((address,port))
+while True:
+    senddata=input('想要发送的数据：')
+    if senddata=='exit':
+        break
+    s.send(senddata.encode())
+    recvdata=s.recv(buffsize).decode('utf-8')
+    print(recvdata,'\n')
+s.close()
